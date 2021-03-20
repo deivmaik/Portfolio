46093a35f9ee1624fd924743753293f059cac0d0
unknown <77588948+deivmaik@users.noreply.github.com>
2021-03-20 18:06:48 +0100

    Fixed grid issue
    -Separated the grid by percentage, seems to be working fine.

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -5,9 +5,47 @@ function Desc() {
     return (
         <div className="desc hide-for-mobile">
             <p className="desc__info">
-            You can hover on each icon to see more info
-            or click them to be redirected to the portfolios.
-          
+
+            About me 
+            -Born in Buenos Aires -Argentina
+            -28 years old
+            -Licensed Architect
+            -Self taught programmer & designer.
+            Feel free to contact me on LinkedIn !
+
+            About me 
+            -Born in Buenos Aires -Argentina
+            -28 years old
+            -Licensed Architect
+            -Self taught programmer & designer.
+            Feel free to contact me on LinkedIn !
+
+            About me 
+            -Born in Buenos Aires -Argentina
+            -28 years old
+            -Licensed Architect
+            -Self taught programmer & designer.
+            Feel free to contact me on LinkedIn !
+            About me 
+            -Born in Buenos Aires -Argentina
+            -28 years old
+            -Licensed Architect
+            -Self taught programmer & designer.
+            Feel free to contact me on LinkedIn !
+
+            About me 
+            -Born in Buenos Aires -Argentina
+            -28 years old
+            -Licensed Architect
+            -Self taught programmer & designer.
+            Feel free to contact me on LinkedIn !
+
+            About me 
+            -Born in Buenos Aires -Argentina
+            -28 years old
+            -Licensed Architect
+            -Self taught programmer & designer.
+            Feel free to contact me on LinkedIn !
             </p>
         </div>
     )
