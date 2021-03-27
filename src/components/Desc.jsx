0e8fe846b63a59086a85d011478abe605d2e7fc1
unknown <77588948+deivmaik@users.noreply.github.com>
2021-03-27 10:05:36 +0100

    Added html & css for skills cover
    -Need to edit the container, it may have to be expanded in size.

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -21,6 +21,33 @@ function Desc({
             <p className="desc__footer">
             {last}
             </p>
+            <div>
+                <div className="square code__one">
+                    <div className="square__image postman"></div>
+                </div>
+                <div className="square code__two">
+                    <div className="square__image mysql"></div>
+                </div>
+                <div className="square code__three">
+                    <div className="square__image typescript"></div>
+                </div>
+                <div className="square code__four">
+                    <div className="square__image aws"></div>
+                </div>
+
+                <div className="square code__fifth">
+                    <div className="square__image html"></div>
+                </div>
+                <div className="square code__sixth">
+                    <div className="square__image sass"></div>
+                </div>
+                <div className="square code__seventh">
+                    <div className="square__image javascript"></div>
+                </div>
+                <div className="square code__eighth">
+                    <div className="square__image react"></div>
+                </div>
+            </div>
         </div>
     )
 }
